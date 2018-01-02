4ef5e9aa3fbc408c5cce49a69f974a31493fa28e
Ahmed Adel Alemam <ahmedemam755@gmail.com>
2018-01-02 22:09:41 +0200

    initial commit
    initial commit

---
+++ js/manipulation.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+  function setHeight() {
+    windowHeight = $(window).innerHeight();
+    $('#map').css('min-height', windowHeight);
+    $('#sidebar').css('min-height', windowHeight);
+  };
+  setHeight();
+
+  $(window).resize(function() {
+    setHeight();
+  });
+});
